e7610b5f55c1b077ff3c5f879fca5714390f9b02
elvynia <elvynia@gmail.com>
2017-10-05 15:31:18 +0200

    Implémentation du component ListArticle.

--- src/app/list-article/list-article.component.ts
+++ src/app/list-article/list-article.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-list-article',
-  templateUrl: './list-article.component.html',
-  styleUrls: ['./list-article.component.css']
+	selector: 'app-list-article',
+	templateUrl: './list-article.component.html',
+	styleUrls: ['./list-article.component.css']
 })
 export class ListArticleComponent implements OnInit {
+	articles: Array<any>;
 
-  constructor() { }
+	constructor() {
+		this.articles = new Array<any>();
+		this.articles.push({
+			title: 'Article n°1',
+			description: 'Super description...'
+		},
+		{
+			title: 'Article n°2',
+			description: 'Autre description...'
+		});
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
+
+	addArticle(event: MouseEvent) {
+		this.articles.push({
+			title: 'Article n°X',
+			description: 'lalalalalala'
+		});
+	}
 
 }
