8665a8376d393eb27a259d51b0567c81e39da731
Elvynia <elvynia@gmail.com>
2018-01-04 09:15:13 +0100

    Reinitialisation pour refaire l'application en v5.

--- src/app/article.ts
+++
@@ -1,9 +0,0 @@
-export class Article {
-	id: number;
-	title: string;
-	description: string;
-
-	constructor(id?: number) {
-		this.id = id;
-	}
-}
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  dataUrl: '/dist/assets/data.json'
-};
