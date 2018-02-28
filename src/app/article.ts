b64faf13dbc64fcd8b161a1969e1626f42d2ef26
Elvynia <elvynia@gmail.com>
2018-02-28 11:04:27 +0100

    Reindentation TS et implémentation de la classe article.

--- src/app/article.ts
+++ src/app/article.ts
@@ -1,2 +1,18 @@
+import * as moment from 'moment';
+
 export class Article {
+	id: number;
+	title: string;
+	description: string;
+	author: string;
+	date: moment.Moment;
+
+	constructor(id: number, title: string, 
+		description?: string, author: string = 'jérémy') {
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.author = author;
+		this.date = moment();
+	}
 }
