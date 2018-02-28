0d913973fa51e43c70c466bb618a2798a6d4fd4c
Elvynia <elvynia@gmail.com>
2018-02-28 16:43:20 +0100

    Ajout d'une liste d'article de test (mock). Ajout d'une liste d'article avec Array et *ngFor. Ajout du module @angular/forms et utilisation de [(ngModel)] pour créer un article.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Article } from './article';
 
 @Component({
 	selector: 'app-root',
@@ -6,5 +9,32 @@ import { Component } from '@angular/core';
 	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-	title = 'Better blog';
+	title: string;
+	articles: Array<Article>;
+	editing: boolean;
+	editArticle: Article;
+
+	constructor() {
+		this.editing = false;
+		this.editArticle = new Article(0, '');
+		this.title = 'Better blog';
+		this.articles = new Array();
+		this.articles.push(new Article(99, 'Article de test', 
+			'Super description...'));
+	}
+
+	addArticle() {
+		this.editing = true;
+	}
+
+	backToList() {
+		setTimeout(() => this.editing = false);
+	}
+
+	saveArticle(myForm: NgForm) {
+		// Utilisation de JSON pour sérialiser puis déserialiser l'article afin
+		// d'obtenir une nouvelle instance d'objet utilisant une autre adresse mémoire.
+		this.articles.push(JSON.parse(JSON.stringify(this.editArticle)));
+		myForm.resetForm();
+	}
 }
