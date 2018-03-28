a8ed32d3c7ec0545d90f77c52e9ed37d5ee6b21f
Elvynia <elvynia@gmail.com>
2018-03-28 14:32:43 +0200

    Formattage des sources avec tabulation=4espaces.

--- src/app/blog-lib/navbar/navbar.component.ts
+++ src/app/blog-lib/navbar/navbar.component.ts
@@ -1,20 +1,20 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'blog-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+	selector: 'blog-navbar',
+	templateUrl: './navbar.component.html',
+	styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  @Input() title: string;
-  @Output() onNavigate: EventEmitter<string>;
+	@Input() title: string;
+	@Output() onNavigate: EventEmitter<string>;
 
-  constructor() {
-    this.title = "Default Title...";
-    this.onNavigate = new EventEmitter();
-  }
+	constructor() {
+		this.title = "Default Title...";
+		this.onNavigate = new EventEmitter();
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
